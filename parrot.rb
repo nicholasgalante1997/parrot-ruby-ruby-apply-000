f4e137fcd4bc4d0712811cba981930b04b389b1c
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-09 23:44:49 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-def parrot(phrase = "Squawk!"
+def parrot(phrase = Squawk!)
   puts phrase
-  returns phrase
-  end
-  
-=======
-def parrot(phrase = "Squawk!")
-  puts phrase
+  return phrase
 end
->>>>>>> 61f1a6d3a9387614a4017c162807bdc421fd59fe
